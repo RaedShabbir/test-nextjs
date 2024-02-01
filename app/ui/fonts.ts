44dde54ccde5157fe3c3e1c1da47f8ea786bc4fb
Raed Shabbir <raedshabbir@gmail.com>
2024-02-01 22:06:01 +0000

    feat: backing up updates

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Orbitron } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({weight: ['400', '700'], subsets: ['latin'] });
+export const orbitron = Orbitron({ subsets: ['latin'] });
